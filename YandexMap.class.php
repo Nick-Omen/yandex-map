4798bcb3ba6b665e9b95001bb1d8a301905d5c9a
Aleksey Novikov <Novikov_9292@mail.ru>
2016-10-14 14:21:59 +0600

    Сохранение данных Front-end
    Сохранение карты,  разработка функции построения json

--- YandexMap.class.php
+++ YandexMap.class.php
@@ -13,6 +13,7 @@ class YandexMap
      */
     public static function init()
     {
+
         self::register_scripts();
     }
 
@@ -23,8 +24,93 @@ class YandexMap
     {
         wp_register_script('yandex-map-class', YMAP_PLUGIN_URL . '_inc/yandex-map.class.js', array('jquery'), null,
             true);
-        wp_register_script('yandex-map', "https://api-maps.yandex.ru/2.1/?lang=" . get_locale() . "&onload=fireYandexMapLoaded", false, null, true);
-        wp_register_script('yandex-map-js', YMAP_PLUGIN_URL . '_inc/yandex-map.js', array('jquery', 'yandex-map', 'yandex-map-class'), null,
+        wp_register_script('yandex-map',
+            "https://api-maps.yandex.ru/2.1/?lang=" . get_locale() . "&onload=fireYandexMapLoaded", false, null, true);
+        wp_register_script('yandex-map-js', YMAP_PLUGIN_URL . '_inc/yandex-map.js',
+            array('jquery', 'yandex-map', 'yandex-map-class'), null,
             true);
+    }
+
+
+    /**
+     * Plugin activation
+     */
+    public static function yamap_activation()
+    {
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+        $map_table = $wpdb->prefix . YMAP_TABLE_PREFIX . "maps";
+        $marker_table = $wpdb->prefix . YMAP_TABLE_PREFIX . "markers";
+
+        $sql = "CREATE TABLE $map_table (
+          ID mediumint(9) NOT NULL AUTO_INCREMENT,
+          Zoom tinyint(1) NOT NULL,
+          Title varchar(255)NOT NULL,
+          Marker_IDs mediumint(9) NOT NULL,
+          Json mediumtext NOT NULL,
+          PRIMARY KEY (`ID`)
+        ) $charset_collate;";
+        dbDelta($sql);
+
+        $sql = "CREATE TABLE $marker_table (
+          ID mediumint(9) NOT NULL AUTO_INCREMENT,
+          Icon varchar(100) NOT NULL,
+          Title varchar(255)NOT NULL,
+          Content text NOT NULL,
+          Type varchar(100) NOT NULL,
+          Preset varchar(100) NOT NULL,
+          PRIMARY KEY (`ID`)
+        ) $charset_collate;";
+        dbDelta($sql);
+    }
+
+    /**
+     * Plugin deactivation
+     */
+    public static function yamap_deactivate()
+    {
+        global $wpdb;
+        $map_table = $wpdb->prefix . YMAP_TABLE_PREFIX . "maps";
+        $marker_table = $wpdb->prefix . YMAP_TABLE_PREFIX . "markers";
+
+        $sql = "DROP TABLE IF EXISTS $map_table";
+        $wpdb->query($sql);
+        $sql = "DROP TABLE IF EXISTS $marker_table";
+        $wpdb->query($sql);
+    }
+
+    /**
+     *  Add map
+     */
+    public static function add_map(array $array)
+    {
+        global $wpdb;
+
+        $map_table = $wpdb->prefix . YMAP_TABLE_PREFIX . "maps";
+
+        $coordinates = array(
+            'lat' => $array['lat'],
+            'lon' => $array['lon']
+        );
+        $json = self::built_map_json($coordinates, 'coordinates');
+
+        $data = array('Zoom' => $array['zoom'] ?: 13, 'Title' => $array['title'], 'Json' => $json);
+        $format = array('%d','%s', '%s');
+
+        return $wpdb->insert($map_table,$data,$format);
+    }
+
+    public static function built_map_json(array $data, $key, $map_id = false)
+    {
+        $out = array();
+        if (!$map_id) {
+            $out[$key] = $data;
+        }
+
+        $json = json_encode($out, true);
+
+        return $json;
     }
 }
--- yandex-map.php
+++ yandex-map.php
@@ -19,13 +19,24 @@ define('YMAP_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('YMAP_FOLDER_NAME', dirname(plugin_basename(__FILE__)));
 define('YMAP_PLUGIN_URL', plugins_url() . '/' . YMAP_FOLDER_NAME . '/');
 define('YMAP_PLUGIN_VERSION', '0.1');
+define('YMAP_TABLE_PREFIX','dp_');
+
 
 // Initialize a plugin
 require_once(YMAP_PLUGIN_DIR . 'YandexMap.class.php');
+register_activation_hook( __FILE__, array( 'YandexMap', 'yamap_activation' ));
+register_deactivation_hook( __FILE__, array( 'YandexMap', 'yamap_deactivate' ));
 add_action('init', array('YandexMap', 'init'));
 
 if (is_admin()) {
     // Initialize admin size of the plugin
     require_once YMAP_PLUGIN_DIR . 'admin' . YMAP_DS . 'YandexMapAdmin.class.php';
     add_action('init', array('YandexMapAdmin', 'init'));
+    // debug tool
+        $data['zoom'] = 7;
+        $data['title'] = 'title';
+        $data['lat'] = '234';
+        $data['lon'] = '567';
+        print_r(YandexMap::add_map($data));exit;
+    // debug tool end
 }
